c420190a01e8745e5e3fe005b7fd897c4a16910f
tropicbliss <tropicbliss@protonmail.com>
2024-08-10 15:51:50 +0800

    Securing the API

--- packages/functions/src/delete.ts
+++ packages/functions/src/delete.ts
@@ -6,15 +6,15 @@ import { DeleteCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 const dynamoDb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
 export const main = Util.handler(async (event) => {
-  const params = {
-    TableName: Resource.Notes.name,
-    Key: {
-      userId: "123", // The id of the author
-      noteId: event?.pathParameters?.id, // The id of the note from the path
-    },
-  };
+    const params = {
+        TableName: Resource.Notes.name,
+        Key: {
+            userId: event.requestContext.authorizer?.iam.cognitoIdentity.identityId, // The id of the author
+            noteId: event?.pathParameters?.id, // The id of the note from the path
+        },
+    };
 
-  await dynamoDb.send(new DeleteCommand(params));
+    await dynamoDb.send(new DeleteCommand(params));
 
-  return JSON.stringify({ status: true });
+    return JSON.stringify({ status: true });
 });
